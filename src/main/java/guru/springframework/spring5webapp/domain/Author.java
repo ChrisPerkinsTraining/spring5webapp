bfb71fba3c09d85a543df17eb8ab81250f85f4ff
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-03-30 14:11:23 +0200

    Removed the Set<Book> from the author and Set<Author> from the Author and Book classes respectively.

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -17,15 +18,14 @@ public class Author {
      *  Set out the Author side of the database mapping
      */
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
     }
 
     public Long getId() {
